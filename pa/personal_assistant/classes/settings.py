84e45b1d208288d4201bdcd3458f39656cfe61a7
Oleh Davydenko <oleg.davidenko@gmail.com>
2024-02-20 10:56:07 +0200

    Final release with make packet

--- pa/personal_assistant/classes/settings.py
+++ pa/personal_assistant/classes/settings.py
@@ -1,4 +1,6 @@
-filename = "adressbook.pkl"
+from pathlib import Path
+
+filename = Path(Path.home(), "adressbook.pkl")
 PAG = 4
 
-notes_filename = "notes.pkl"
+notes_filename = Path(Path.home(), "notes.pkl")
